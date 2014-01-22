934ef10be0dfdaa432b979ce10c13722e6d54a3b
Hilmar Lapp <hlapp@drycafe.net>
2014-01-22 11:43:03 -0500

    Removing icon-less SOCIAL links.
    These aren't really "SOCIAL" but more like professional profiles, and they are
    now covered in the About page.

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,11 +28,6 @@ SOCIAL = (('Github', 'http://github.com/hlapp'),
           ('Google+', 'http://plus.google.com/+HilmarLapp/'),
           ('Twitter', 'http://twitter.com/hlapp'),
           ('LinkedIn', 'http://linkedin.com/in/hlapp'),
-          ('ORCID', 'http://orcid.org/0000-0001-9107-0714'),
-          ('Mendeley', 'http://www.mendeley.com/profiles/hilmar-lapp'),
-          ('ImpactStory', 'http://impactstory.org/hlapp'),
-          ('Google Scholar', 'http://scholar.google.com/citations?user=CK6Qg7gAAAAJ'),
-          ('Slideshare', 'http://www.slideshare.net/hlapp/presentations'),
           )
 
 DEFAULT_PAGINATION = 10
